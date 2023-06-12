74466240945035edf27bf8598f2eab13877f9493
Marquis Sampson <marquis4484@gmail.com>
2023-06-12 14:08:16 -0400

    Changes to home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,13 +3,13 @@ import { ArrowPathIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
    return (
-      <div>
+      <div className="flex flex-col items-center">
          <p>Hello,my name is</p>
          <span>Marquis Sampson</span>
          <div className="border-t-[3px]"></div>
          <span>Software Developer</span>
          <p>Welcome to my website</p>
-         <p>While you are exploring this webpage please...</p>
+         <p>While are exploring this webpage please...</p>
          <p>Feel free to</p>
          <p>set the mood</p>
          <button>
