3873cd8c99b51b94ade74dd562e4aa801f77c1c8
Marquis Sampson <marquis4484@gmail.com>
2023-06-15 15:38:12 -0400

    Working on input

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -6,12 +6,17 @@ export default function ContectMe() {
          <h1>Thank you for visiting my webpage!</h1>
          <p>Enter your email and leave a message so we can get in touch!</p>
          <form className="flex flex-col" action="">
-            <input type="text" className="bg-[#D9D9D9] my-[30px] " />
+            <input
+               type="email"
+               placeholder="Email"
+               className="bg-[#D9D9D9] my-[30px] "
+            />
             <input
                type="text"
                className="bg-[#D9D9D9] w-[260px] h-[200px] p-[10px] mb-[30px]"
                placeholder="Message"
             />
+            <button>Send</button>
          </form>
       </div>
    );
