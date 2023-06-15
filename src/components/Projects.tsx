be13b3cdf5f0cb5b363698e25bcc5a200ead8f9f
Marquis Sampson <marquis4484@gmail.com>
2023-06-15 15:43:33 -0400

    project update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Projects() {
-   return <div>Projects</div>;
+   return (
+      <div>
+         <h1>Here’s what I’ve been working on.</h1>
+      </div>
+   );
 }
