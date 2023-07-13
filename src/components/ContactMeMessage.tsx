d891e0c6b8ee04115cb18f8fb7b350e8b2aca38c
Marquis Sampson <marquis4484@gmail.com>
2023-07-13 15:44:18 -0400

    Added Slider

--- src/components/ContactMeMessage.tsx
+++ src/components/ContactMeMessage.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { XCircleIcon } from "@heroicons/react/24/solid";
+import Confetti from "react-confetti";
 
 interface ContactMeValues {
    message: boolean;
@@ -7,12 +8,54 @@ interface ContactMeValues {
 }
 
 function ContactMeMessage({ message, setMessage }: ContactMeValues) {
+   const [windowDimension, setWindowDimension] = useState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+   });
+
+   const detectSize = () => {
+      setWindowDimension({
+         width: window.innerWidth,
+         height: window.innerHeight,
+      });
+   };
+
+   useEffect(() => {
+      window.addEventListener("resize", detectSize);
+      return () => {
+         window.removeEventListener("resize", detectSize);
+      };
+   }, [windowDimension]);
+
    const handleClose = () => {
       setMessage(false);
    };
 
+   let width: number = 42;
+   let height: number = 42;
+
    return (
       <div>
+         {message && (
+            <Confetti
+               numberOfPieces={200}
+               width={windowDimension.width}
+               height={windowDimension.height}
+               tweenDuration={1000}
+            />
+         )}
+         <div
+            className={`${
+               message ? "z-[4] opacity-100 " : " z-[-2] opacity-0  "
+            } " bg-[#D9D9D9] fixed py-[20px] px-[30px] rounded-[50px] text-center left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] w-[60%] h-[60%] duration-300 "`}
+         >
+            <h1>Your Message is Appreciated!</h1>
+
+            <p>
+               It may take a while to respond to your message. Nevertheless, I
+               will respond to your message as soon as possible!
+            </p>
+         </div>
          <div
             onClick={handleClose}
             className={`${
@@ -21,7 +64,7 @@ function ContactMeMessage({ message, setMessage }: ContactMeValues) {
                   : "z-[-1] opacity-0"
             } h-screen w-screen bg-[#000000] fixed duration-[400ms] `}
          />
-         <h1>Your Message is Appreciated!</h1>
+
          <button
             onClick={handleClose}
             className={`${message ? "opacity-100 z-[2]" : "opacity-0 z-[-1]"} 
@@ -29,10 +72,6 @@ function ContactMeMessage({ message, setMessage }: ContactMeValues) {
          >
             <XCircleIcon className="text-[50px] text-[#000000]" />
          </button>
-         <p>
-            It may take a while to respond to your message. Nevertheless, I will
-            respond to your message as soon as possible!
-         </p>
       </div>
    );
 }
