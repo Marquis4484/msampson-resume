f7bb2831a9fa70a053262b63a0b59a7530176d07
Marquis Sampson <marquis4484@gmail.com>
2023-07-18 16:34:50 -0400

    Working on ContactMe Page

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,14 +1,30 @@
+import emailjs from "emailjs-com";
 import { useState } from "react";
 import ContactMeMessage from "./ContactMeMessage";
 
 export default function ContactMe() {
    const [message, setMessage] = useState(false);
 
-   const handleSubmit = (
-      e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-   ) => {
+   const handleSubmit = (e: any) => {
       e.preventDefault();
       setMessage(true);
+
+      emailjs
+         .sendForm(
+            "process.env.YOUR_SERVICE_ID",
+            "process.env.YOUR_TEMPLATE_ID",
+            e.target,
+            "process.env.YOUR_PUBLIC_KEY"
+         )
+         .then(
+            (result) => {
+               console.log(result.text);
+            },
+            (error) => {
+               console.log(error.text);
+            }
+         );
+      e.target.reset;
    };
 
    return (
@@ -21,7 +37,7 @@ export default function ContactMe() {
             <p className="text-[20px] text-bold text-center pb-[5px]">
                Enter your email and leave a message so we can get in touch!
             </p>
-            <form className="flex flex-col" action="">
+            <form onSubmit={handleSubmit} className="flex flex-col" action="">
                <input
                   type="email"
                   placeholder="Email"
@@ -30,7 +46,7 @@ export default function ContactMe() {
                />
                <textarea
                   placeholder="Leave a quick Message"
-                  className="bg-[#D9D9D9] h-[180px] w-[260px]"
+                  className="bg-[#D9D9D9] h-[280px] w-[260px]"
                   required
                ></textarea>
                <button
