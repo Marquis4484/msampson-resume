b24a3d73ba61fadd3f4bcd1e4b9265a3dd243c8c
Marquis Sampson <marquis4484@gmail.com>
2023-08-09 14:07:44 -0400

    Changes to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
   const [themeIdx, setThemeIdx] = useState(0);
-  // or change this in your useTheme hook
 
   const themes = [
     { name: "Dark" },
