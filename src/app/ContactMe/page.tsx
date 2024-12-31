60b19bb36a2540b03505d0476a0fdbc40c2119f3
Marquis Sampson <marquis4484@gmail.com>
2024-12-31 12:44:52 -0500

    update

--- src/app/ContactMe/page.tsx
+++ src/app/ContactMe/page.tsx
@@ -16,7 +16,7 @@ export default function ContactMe() {
         process.env.NEXT_PUBLIC_YOUR_SERVICE_ID || "",
         process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID || "",
         form.current,
-        process.env.NEXT_PUBLIC_YOUR_PUBLIC_KEY
+        process.env.NEXT_PUBLIC_YOUR_PUBLIC
       )
       .then(
         (result) => {
